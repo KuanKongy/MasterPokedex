00a5f75a76cba7eb0b3fc9b4a7f451dcfe566f04
KuanKongy <khanhpronam@gmail.com>
2025-04-04 05:49:00 +0000

    Fix: Resolve React hook errors
    Fixes TypeError related to null object when evaluating 'dispatcher.useEffect' and 'dispatcher.useState'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -12,6 +13,7 @@ import Map from "./pages/Map";
 import Trainer from "./pages/Trainer";
 import Items from "./pages/Items";
 
+// Create a client
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -21,26 +23,30 @@ const queryClient = new QueryClient({
   },
 });
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Index />} />
-            <Route path="/pokemon/:id" element={<PokemonDetail />} />
-            <Route path="/map" element={<Map />} />
-            <Route path="/trainer" element={<Trainer />} />
-            <Route path="/items" element={<Items />} />
-          </Route>
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<Index />} />
+                <Route path="/pokemon/:id" element={<PokemonDetail />} />
+                <Route path="/map" element={<Map />} />
+                <Route path="/trainer" element={<Trainer />} />
+                <Route path="/items" element={<Items />} />
+              </Route>
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </TooltipProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
+};
 
 export default App;
